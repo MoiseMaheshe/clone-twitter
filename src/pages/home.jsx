e23c3e70bd9f8900276d963d89808e41e08dc3e3
mmaheshe3 <27032271-mmaheshe3@users.noreply.replit.com>
2023-12-06 09:49:20 +0000

    twitter-clone
    sprint1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Header from '../components/header';
 function Home() {
-    return (
-        <main className="timeline">
-            <h1>Timeline here</h1>
-        </main>
-
-    );
+  return (
+    <main className="timeline">
+      <h1></h1>
+      <Header />
+    </main>
+  );
 }
 
 export default Home;
